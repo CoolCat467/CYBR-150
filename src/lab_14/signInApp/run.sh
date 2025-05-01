765d49aa3af2b08e600d64d1c76430eb9b247e40
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2025-05-01 16:38:59 -0500

    Way better way to run application

--- src/lab_14/signInApp/run.sh
+++ src/lab_14/signInApp/run.sh
@@ -2,6 +2,77 @@
 # -*- coding: utf-8 -*-
 # Programmed by CoolCat467
 
-bash mvnw package
-# -javaagent:$HOME/Applications/idea-IC-251.23774.435/lib/idea_rt.jar=42813
-java -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath $HOME/.m2/repository/org/openjfx/javafx-controls/21/javafx-controls-21.jar:$HOME/.m2/repository/org/openjfx/javafx-graphics/21/javafx-graphics-21.jar:$HOME/.m2/repository/org/openjfx/javafx-base/21/javafx-base-21.jar:$HOME/.m2/repository/org/openjfx/javafx-fxml/21/javafx-fxml-21.jar:$HOME/.m2/repository/org/openjfx/javafx-media/21/javafx-media-21.jar:$HOME/.m2/repository/org/jetbrains/kotlin/kotlin-stdlib-common/1.8.10/kotlin-stdlib-common-1.8.10.jar:$HOME/.m2/repository/org/jetbrains/annotations/13.0/annotations-13.0.jar -p $HOME/.m2/repository/org/openjfx/javafx-base/21/javafx-base-21-linux.jar:$HOME/.m2/repository/org/controlsfx/controlsfx/11.2.1/controlsfx-11.2.1.jar:$HOME/.m2/repository/com/github/almasb/fxgl-io/17.3/fxgl-io-17.3.jar:$HOME/.m2/repository/org/openjfx/javafx-media/21/javafx-media-21-linux.jar:$HOME/.m2/repository/com/gluonhq/attach/storage/4.0.17/storage-4.0.17.jar:$HOME/.m2/repository/com/github/almasb/fxgl-core/17.3/fxgl-core-17.3.jar:$HOME/.m2/repository/com/gluonhq/attach/util/4.0.17/util-4.0.17.jar:$HOME/.m2/repository/org/openjfx/javafx-fxml/21/javafx-fxml-21-linux.jar:$HOME/.m2/repository/org/openjfx/javafx-graphics/21/javafx-graphics-21-linux.jar:$HOME/.m2/repository/org/jetbrains/kotlin/kotlin-stdlib/1.8.10/kotlin-stdlib-1.8.10.jar:$HOME/.m2/repository/com/github/almasb/fxgl-scene/17.3/fxgl-scene-17.3.jar:$HOME/.m2/repository/org/openjfx/javafx-controls/21/javafx-controls-21-linux.jar:$HOME/.m2/repository/com/github/almasb/fxgl-entity/17.3/fxgl-entity-17.3.jar:$HOME/.m2/repository/com/dlsc/formsfx/formsfx-core/11.6.0/formsfx-core-11.6.0.jar:$HOME/.m2/repository/com/gluonhq/attach/audio/4.0.17/audio-4.0.17.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.14.2/jackson-databind-2.14.2.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.14.2/jackson-annotations-2.14.2.jar:$HOME/.m2/repository/com/github/almasb/fxgl-gameplay/17.3/fxgl-gameplay-17.3.jar:$HOME/.m2/repository/org/kordamp/bootstrapfx/bootstrapfx-core/0.4.0/bootstrapfx-core-0.4.0.jar:$HOME/.m2/repository/com/gluonhq/attach/lifecycle/4.0.17/lifecycle-4.0.17.jar:$HOME/.m2/repository/com/github/almasb/fxgl/17.3/fxgl-17.3.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.14.2/jackson-core-2.14.2.jar:"$(pwd)/target/classes" -m com.signinapp.signin/com.signinapp.signin.HelloApplication
+# bash mvnw package
+# # -javaagent:$HOME/Applications/idea-IC-251.23774.435/lib/idea_rt.jar=42813
+# java -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath $HOME/.m2/repository/org/openjfx/javafx-controls/21/javafx-controls-21.jar:$HOME/.m2/repository/org/openjfx/javafx-graphics/21/javafx-graphics-21.jar:$HOME/.m2/repository/org/openjfx/javafx-base/21/javafx-base-21.jar:$HOME/.m2/repository/org/openjfx/javafx-fxml/21/javafx-fxml-21.jar:$HOME/.m2/repository/org/openjfx/javafx-media/21/javafx-media-21.jar:$HOME/.m2/repository/org/jetbrains/kotlin/kotlin-stdlib-common/1.8.10/kotlin-stdlib-common-1.8.10.jar:$HOME/.m2/repository/org/jetbrains/annotations/13.0/annotations-13.0.jar -p $HOME/.m2/repository/org/openjfx/javafx-base/21/javafx-base-21-linux.jar:$HOME/.m2/repository/org/controlsfx/controlsfx/11.2.1/controlsfx-11.2.1.jar:$HOME/.m2/repository/com/github/almasb/fxgl-io/17.3/fxgl-io-17.3.jar:$HOME/.m2/repository/org/openjfx/javafx-media/21/javafx-media-21-linux.jar:$HOME/.m2/repository/com/gluonhq/attach/storage/4.0.17/storage-4.0.17.jar:$HOME/.m2/repository/com/github/almasb/fxgl-core/17.3/fxgl-core-17.3.jar:$HOME/.m2/repository/com/gluonhq/attach/util/4.0.17/util-4.0.17.jar:$HOME/.m2/repository/org/openjfx/javafx-fxml/21/javafx-fxml-21-linux.jar:$HOME/.m2/repository/org/openjfx/javafx-graphics/21/javafx-graphics-21-linux.jar:$HOME/.m2/repository/org/jetbrains/kotlin/kotlin-stdlib/1.8.10/kotlin-stdlib-1.8.10.jar:$HOME/.m2/repository/com/github/almasb/fxgl-scene/17.3/fxgl-scene-17.3.jar:$HOME/.m2/repository/org/openjfx/javafx-controls/21/javafx-controls-21-linux.jar:$HOME/.m2/repository/com/github/almasb/fxgl-entity/17.3/fxgl-entity-17.3.jar:$HOME/.m2/repository/com/dlsc/formsfx/formsfx-core/11.6.0/formsfx-core-11.6.0.jar:$HOME/.m2/repository/com/gluonhq/attach/audio/4.0.17/audio-4.0.17.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.14.2/jackson-databind-2.14.2.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.14.2/jackson-annotations-2.14.2.jar:$HOME/.m2/repository/com/github/almasb/fxgl-gameplay/17.3/fxgl-gameplay-17.3.jar:$HOME/.m2/repository/org/kordamp/bootstrapfx/bootstrapfx-core/0.4.0/bootstrapfx-core-0.4.0.jar:$HOME/.m2/repository/com/gluonhq/attach/lifecycle/4.0.17/lifecycle-4.0.17.jar:$HOME/.m2/repository/com/github/almasb/fxgl/17.3/fxgl-17.3.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.14.2/jackson-core-2.14.2.jar:"$(pwd)/target/classes" -m com.signinapp.signin/com.signinapp.signin.HelloApplication
+
+
+# java
+# -Dfile.encoding=UTF-8
+# -Dsun.stdout.encoding=UTF-8
+# -Dsun.stderr.encoding=UTF-8
+# -classpath $HOME/.m2/repository/org/openjfx/javafx-controls/21/javafx-controls-21.jar:$HOME/.m2/repository/org/openjfx/javafx-graphics/21/javafx-graphics-21.jar:$HOME/.m2/repository/org/openjfx/javafx-base/21/javafx-base-21.jar:$HOME/.m2/repository/org/openjfx/javafx-fxml/21/javafx-fxml-21.jar:$HOME/.m2/repository/org/openjfx/javafx-media/21/javafx-media-21.jar:$HOME/.m2/repository/org/jetbrains/kotlin/kotlin-stdlib-common/1.8.10/kotlin-stdlib-common-1.8.10.jar:$HOME/.m2/repository/org/jetbrains/annotations/13.0/annotations-13.0.jar
+# -p $HOME/.m2/repository/org/openjfx/javafx-base/21/javafx-base-21-linux.jar:$HOME/.m2/repository/org/controlsfx/controlsfx/11.2.1/controlsfx-11.2.1.jar:$HOME/.m2/repository/com/github/almasb/fxgl-io/17.3/fxgl-io-17.3.jar:$HOME/.m2/repository/org/openjfx/javafx-media/21/javafx-media-21-linux.jar:$HOME/.m2/repository/com/gluonhq/attach/storage/4.0.17/storage-4.0.17.jar:$HOME/.m2/repository/com/github/almasb/fxgl-core/17.3/fxgl-core-17.3.jar:$HOME/.m2/repository/com/gluonhq/attach/util/4.0.17/util-4.0.17.jar:$HOME/.m2/repository/org/openjfx/javafx-fxml/21/javafx-fxml-21-linux.jar:$HOME/.m2/repository/org/openjfx/javafx-graphics/21/javafx-graphics-21-linux.jar:$HOME/.m2/repository/org/jetbrains/kotlin/kotlin-stdlib/1.8.10/kotlin-stdlib-1.8.10.jar:$HOME/.m2/repository/com/github/almasb/fxgl-scene/17.3/fxgl-scene-17.3.jar:$HOME/.m2/repository/org/openjfx/javafx-controls/21/javafx-controls-21-linux.jar:$HOME/.m2/repository/com/github/almasb/fxgl-entity/17.3/fxgl-entity-17.3.jar:$HOME/.m2/repository/com/dlsc/formsfx/formsfx-core/11.6.0/formsfx-core-11.6.0.jar:$HOME/.m2/repository/com/gluonhq/attach/audio/4.0.17/audio-4.0.17.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.14.2/jackson-databind-2.14.2.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.14.2/jackson-annotations-2.14.2.jar:$HOME/.m2/repository/com/github/almasb/fxgl-gameplay/17.3/fxgl-gameplay-17.3.jar:$HOME/.m2/repository/org/kordamp/bootstrapfx/bootstrapfx-core/0.4.0/bootstrapfx-core-0.4.0.jar:$HOME/.m2/repository/com/gluonhq/attach/lifecycle/4.0.17/lifecycle-4.0.17.jar:$HOME/.m2/repository/com/github/almasb/fxgl/17.3/fxgl-17.3.jar:$HOME/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.14.2/jackson-core-2.14.2.jar:"$(pwd)/target/classes"
+# -m com.signinapp.signin/com.signinapp.signin.HelloApplication
+
+
+# # Variables
+# PROJECT_DIR=$(pwd)
+# PACKAGE_NAME="com.signinapp.signin"
+# CLASS_NAME="HelloApplication"
+
+# MAIN_CLASS="$PACKAGE_NAME.$CLASS_NAME"
+# MODULE="$PACKAGE_NAME/$MAIN_CLASS"
+# # ARTIFACT_NAME="signIn-1.0-SNAPSHOT.jar"
+
+# PROJECT_TARGET="$PROJECT_DIR/target"
+# PROJECT_CLASSES="$PROJECT_TARGET/classes"
+# CLASSPATH_FILE="$PROJECT_DIR/target/classpath.txt"
+
+
+
+# # Compile the project
+# echo "Compiling project"
+# bash mvnw package
+
+# # Check if the compilation was successful
+# if [ $? -ne 0 ]; then
+#     echo "Compilation failed."
+#     exit 1
+# fi
+
+# echo "Building project classpath"
+
+# # Get the classpath for the application
+# bash mvnw dependency:build-classpath -Dmdep.outputFile="$CLASSPATH_FILE"
+
+# # Check if the compilation was successful
+# if [ $? -ne 0 ]; then
+#     echo "Building classpath failed."
+#     exit 1
+# fi
+
+# CLASSPATH=$(cat "$CLASSPATH_FILE"):\""$PROJECT_CLASSES"\"
+
+# echo ""
+# echo "CLASSPATH = $CLASSPATH"
+# echo ""
+# echo "MODULE = $MODULE"
+# echo ""
+
+echo "Running application"
+
+# Run the application
+# java \
+#     -Dfile.encoding=UTF-8 \
+#     -Dsun.stdout.encoding=UTF-8 \
+#     -Dsun.stderr.encoding=UTF-8 \
+#     -classpath "$CLASSPATH" \
+#     -m "$MODULE"
+
+bash mvnw javafx:run
+
+if [ $? -ne 0 ]; then
+    echo "Application failed to run."
+    exit 1
+fi
